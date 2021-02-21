2b05ce7ec413fa1ed33e260c354aae4975b9c08e
janniksinz <j.sinz@gmx.de>
2021-02-21 21:16:44 +0100

    feat: add first test endpoint
    this uses the micronaut annotations to define a endpoint to the api
    There is also a yml file for kubernetes deployment
    
    closes #4

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -4,11 +4,14 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+import javax.inject.Inject;
+
 @Controller("/")
 public class HelloController {
 
     private final HelloService helloService;
 
+    @Inject
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -3,16 +3,15 @@ package com.example;
 public class User {
     private String name;
 
-
     public User(String name) {
         this.name = name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
-    public String getName(){
-        return this.name;
+    public void setName(String name) {
+        this.name = name;
     }
 }
