f314f5852d358d7e61a21f7bc922800dde188b68
janniksinz <j.sinz@gmx.de>
2021-02-28 20:59:31 +0100

    fix: fix SurveyService
    before the survey was created in the controller which is not clean. now we have static methods in the service, that will crawl the data from the db in the future and creates the survey.

--- src/main/java/com/partycipate/controllers/SurveyController.java
+++ src/main/java/com/partycipate/controllers/SurveyController.java
@@ -4,7 +4,6 @@ import com.partycipate.models.Survey;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
@@ -18,7 +17,7 @@ public class SurveyController {
     @Produces(MediaType.TEXT_PLAIN)
     public Survey getSurvey(int id){
 
-        return new Survey.Builder().id(id).name("GetSurvey").build();
+        return SurveyService.getSurvey(id);
     }
 
 }
--- src/main/java/com/partycipate/services/SurveyService.java
+++ src/main/java/com/partycipate/services/SurveyService.java
@@ -2,26 +2,22 @@ package com.partycipate.services;
 
 import com.partycipate.models.Survey;
 
-import javax.annotation.Nullable;
 import javax.inject.Singleton;
 
 @Singleton
 public class SurveyService {
-    @Nullable
-    private int id;
-    private String name;
 
-    public Survey createSurvey(String name){
+    public static Survey createSurvey(String name){
         Survey survey;
         survey = new Survey.Builder().id(1).name("MyFirstSurvey").build();
         return survey;
     }
 
-    public Survey getSurvey(int id){
+    public static Survey getSurvey(int id){
         //get survey from db with id
         //obviously id and name need to be inserted from db data
         // like Survey.Builder().id(data.id).name(data.name).build();
-        Survey survey = new Survey.Builder().id(1).name("Name from Database").build();
+        Survey survey = new Survey.Builder().id(id).name("Name from Database").build();
         return survey;
     }
 
