17ad967fe1d3e8738b021b9a0c073939fc8dcc62
janniksinz <j.sinz@gmx.de>
2021-02-28 21:07:48 +0100

    add new endpoint for SurveyController

--- src/main/java/com/partycipate/controllers/SurveyController.java
+++ src/main/java/com/partycipate/controllers/SurveyController.java
@@ -2,22 +2,33 @@ package com.partycipate.controllers;
 
 import com.partycipate.models.Survey;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.*;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+
+
 import com.partycipate.services.SurveyService;
+
 
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/survey")
 public class SurveyController {
 
     @Get("/{id}")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    //@Header
+    //@Body
     public Survey getSurvey(int id){
-
         return SurveyService.getSurvey(id);
+    }
+
+    @Get("/create")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Survey createSurvey(String name){
+        //create survey with service here
+        return SurveyService.createSurvey(name);
     }
 
 }
