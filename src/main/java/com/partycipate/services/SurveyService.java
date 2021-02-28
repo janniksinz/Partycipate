3e939822512a9b0247966d4b443cf13e2c9ff17b
janniksinz <j.sinz@gmx.de>
2021-02-28 21:02:03 +0100

    minor changes in SurveyService
    - optimized code
    - revised comments

--- src/main/java/com/partycipate/services/SurveyService.java
+++ src/main/java/com/partycipate/services/SurveyService.java
@@ -8,14 +8,15 @@ import javax.inject.Singleton;
 public class SurveyService {
 
     public static Survey createSurvey(String name){
-        Survey survey;
-        survey = new Survey.Builder().id(1).name("MyFirstSurvey").build();
+        Survey survey = new Survey.Builder().name(name).build();
+        //insert the data in the db with methods from the db_service
         return survey;
     }
 
     public static Survey getSurvey(int id){
         //get survey from db with id
-        //obviously id and name need to be inserted from db data
+        //obviously id and name need to be inserted from db.
+        // data needs to be fetched with the db_service
         // like Survey.Builder().id(data.id).name(data.name).build();
         Survey survey = new Survey.Builder().id(id).name("Name from Database").build();
         return survey;
