6baa5a014af785f121cf2722409f096dc00cf415
Andreas <wi19165@lehre.dhbw-stuttgart.de>
2021-03-15 14:59:48 +0100

    added ResultSet for AnswerPossibilities

--- src/main/java/com/partycipate/Partycipate/controller/ParticipantController.java
+++ src/main/java/com/partycipate/Partycipate/controller/ParticipantController.java
@@ -2,15 +2,10 @@ package com.partycipate.Partycipate.controller;
 
 import com.partycipate.Partycipate.model.Answer;
 import com.partycipate.Partycipate.model.Result;
-import com.partycipate.Partycipate.model.SendAnswer;
-import com.partycipate.Partycipate.model.Survey;
 import com.partycipate.Partycipate.service.AnswerService;
 import com.partycipate.Partycipate.service.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/participant")
@@ -31,9 +26,9 @@ public class ParticipantController {
     }
 
     //getBasicResults
-    @GetMapping("/results")
-    public Result getBasicResults(){
-        return answerService.results();
+    @GetMapping("/results/{id}")
+    public Result getBasicResults(@PathVariable ("id")int id){
+        return answerService.results(id);
     }
 
     //sendAnswer
--- src/main/java/com/partycipate/Partycipate/service/AnswerService.java
+++ src/main/java/com/partycipate/Partycipate/service/AnswerService.java
@@ -1,15 +1,14 @@
 package com.partycipate.Partycipate.service;
 
 import com.partycipate.Partycipate.model.Answer;
+import com.partycipate.Partycipate.model.MCAnswerContent;
 import com.partycipate.Partycipate.model.Result;
 import com.partycipate.Partycipate.repository.AnswerRepository;
-import com.partycipate.Partycipate.repository.SurveyElementRepository;
-import com.partycipate.Partycipate.repository.SurveyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Set;
 
 @Service
@@ -23,6 +22,10 @@ public class AnswerService {
         this.answerRepository = answerRepository;
     }
 
+    @Autowired
+    private AnswerPossibilityService answerPossibilityService;
+
+
     //getAnswersByElementId
     public Set<Answer> getAnswersByElementId(int id){
         Set<Answer> answers = answerRepository.getAnswersByElementId(id);
@@ -30,10 +33,32 @@ public class AnswerService {
     }
 
 
-    public Result results(){
+    public Result results(int element_id){
         //calculate all the answers to one Result to send back to the Frontend
-        return new Result();
+        int count = answerPossibilityService.getCountOfAnswersPossibilities(element_id);
+        //brauche Set von Answers; ArrayList initialisen mit Laenge count; Iterator durchgehen und ArrayList hochzaehlen;
+        // ArrayList in Result setzen (setResult)
+        Set<Answer> answers = answerRepository.getAnswersByElementId(element_id);
+        ArrayList<Integer>counting_results = new ArrayList<>(count);
+        Iterator<Answer> iterator = answers.iterator();
+        while (iterator.hasNext()){
+            Answer a = iterator.next();
+            Set<MCAnswerContent> mcac = a.getMcAnswerContentSet();
+            Iterator<MCAnswerContent> mcacIterator = mcac.iterator();
+            while(mcacIterator.hasNext()){
+                MCAnswerContent content = mcacIterator.next();
+                int position = content.getId();
+                int value = content.getContent();
+                value += counting_results.get(position);
+                counting_results.add(position, value);
+            }
+        }
+        Result result = new Result();
+        result.setResults(counting_results);
+        return  result;
+
     }
+
 
     
 }
