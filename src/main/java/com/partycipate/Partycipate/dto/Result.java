731c6b6a7c10eb644825050a14127e5537d0b67e
Andreas <wi19165@lehre.dhbw-stuttgart.de>
2021-03-15 17:07:12 +0100

    added ResultClass

--- src/main/java/com/partycipate/Partycipate/dto/Result.java
+++ src/main/java/com/partycipate/Partycipate/dto/Result.java
@@ -1,5 +1,15 @@
 package com.partycipate.Partycipate.dto;
 
-public class Result {
+import java.util.ArrayList;
 
+public class Result {
+    ArrayList<Integer> results = new ArrayList();
+
+    //how many answer possibilities (amount of answers with same survey_element_id)
+    int size;
+    int count_participants;
+
+    public void setResults(ArrayList<Integer> arrayList){
+        this.results = arrayList;
+    }
 }
