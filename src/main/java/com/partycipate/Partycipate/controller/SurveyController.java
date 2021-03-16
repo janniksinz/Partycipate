e496870aad31ccf3d2003c271285a5449a974f00
janniksinz <j.sinz@gmx.de>
2021-03-16 11:43:01 +0100

    add SendElement structure

--- src/main/java/com/partycipate/Partycipate/controller/SurveyController.java
+++ src/main/java/com/partycipate/Partycipate/controller/SurveyController.java
@@ -3,28 +3,25 @@ package com.partycipate.Partycipate.controller;
 
 import com.partycipate.Partycipate.dto.SendElement;
 import com.partycipate.Partycipate.dto.SendSurvey;
+import com.partycipate.Partycipate.model.Survey;
 import com.partycipate.Partycipate.service.SurveyElementService;
 import com.partycipate.Partycipate.service.SurveyService;
-import com.partycipate.Partycipate.model.Survey;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
 
 
 @RestController
 @RequestMapping("/api/survey")
 public class SurveyController {
 
-    private final SurveyElementService surveyElement;
+    @Autowired
+    private final SurveyElementService surveyElementService;
+    @Autowired
     private final SurveyService surveyService;
     @Autowired
-    public SurveyController(SurveyService surveyService, SurveyElementService surveyElement){
+    public SurveyController(SurveyService surveyService, SurveyElementService surveyElementService){
         this.surveyService=surveyService;
-        this.surveyElement = surveyElement;
+        this.surveyElementService = surveyElementService;
     }
 
 
@@ -53,6 +50,12 @@ public class SurveyController {
         //ToDo send Exception in Case of already deleted
         surveyService.deleteSurveybyId(id);
         return id;
+    }
+
+    //addSurveyElement
+    @PostMapping("/element")
+    public int addSurveyElement(@RequestBody SendElement sendElement){
+        return surveyElementService.addSurveyElement(sendElement);
     }
 
 
--- src/main/java/com/partycipate/Partycipate/service/SurveyElementService.java
+++ src/main/java/com/partycipate/Partycipate/service/SurveyElementService.java
@@ -1,13 +1,9 @@
 package com.partycipate.Partycipate.service;
 
 import com.partycipate.Partycipate.dto.SendElement;
-import com.partycipate.Partycipate.dto.SendSurvey;
-import com.partycipate.Partycipate.model.Survey;
 import com.partycipate.Partycipate.model.SurveyElement;
 import com.partycipate.Partycipate.repository.SurveyElementRepository;
-import com.partycipate.Partycipate.repository.SurveyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,6 +21,7 @@ public class SurveyElementService {
     public int addSurveyElement(SendElement sE){
         SurveyElement surveyElement = new SurveyElement.Builder().may_skip(sE.isMay_skip()).position(sE.getPosition()).question(sE.getQuestion()).type(sE.getType()).build();
         surveyElement.setSurvey(surveyService.getSurvey(sE.getSurvey_id()));
+        //ToDo create manual implementation of save to only save surveyElement and not the Survey, too.
         return surveyElementRepository.save(surveyElement).getId();
     }
 
