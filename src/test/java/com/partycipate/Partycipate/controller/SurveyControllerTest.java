cfab91300499021c8c54f0382d710469dd7c7f3f
janniksinz <j.sinz@gmx.de>
2021-04-01 11:45:48 +0200

    add REST test for getAlSurveys - HTTPStatus Code

---
+++ src/test/java/com/partycipate/Partycipate/controller/SurveyControllerTest.java
@@ -0,0 +1,51 @@
+package com.partycipate.Partycipate.controller;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SurveyControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addSurvey() {
+    }
+
+    @Test
+    void getAllSurveys() throws IOException {
+//        Given
+        HttpUriRequest request = new HttpGet("http://localhost:8088/api/survey");
+//        When
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+//        Then
+        assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
+    }
+
+    @Test
+    void getSurvey() {
+    }
+
+    @Test
+    void deleteSurveybyId() {
+    }
+
+    @Test
+    void addSurveyElement() {
+    }
+}
