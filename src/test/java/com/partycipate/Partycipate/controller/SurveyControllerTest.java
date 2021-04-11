b670f029870d0bd0ce25d1b00a280c0413793902
Jarg <wi19225@lehre.dhbw-stuttgart.de>
2021-04-11 16:16:31 +0200

    SurveyControllerTest addSurvey does self delete + further test improvements + retrieveResourceFromResponse method

--- src/test/java/com/partycipate/Partycipate/controller/SurveyControllerTest.java
+++ src/test/java/com/partycipate/Partycipate/controller/SurveyControllerTest.java
@@ -1,27 +1,44 @@
 package com.partycipate.Partycipate.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.partycipate.Partycipate.repository.SurveyRepository;
+import com.partycipate.Partycipate.repository.UserRepository;
 import com.partycipate.Partycipate.service.SurveyService;
+import com.partycipate.Partycipate.service.UserService;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.*;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SurveyControllerTest {
 
+    @InjectMocks
+    SurveyService surveyService;
+
+    @Mock
+    SurveyRepository surveyRepository;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
+        MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
@@ -30,37 +47,43 @@ class SurveyControllerTest {
 
 
     @Test
-    void addSurvey() throws UnsupportedEncodingException {
+    void addSurvey() throws IOException {
         //ToDo add (header for auth
-        //        Given
+    //Given
+        //create request with body & user_id=1
         HttpUriRequest request = null;
+        StringEntity stringE = new StringEntity("{\"creation_date\": \"creation_date\", " +
+               "\"title\": \"title\", " +
+                "\"user_id\": 1, " +
+                "\"elements\": null}");
+        stringE.setContentType("application/json;charset=utf-8");
+        request = RequestBuilder.create("POST")
+                .setUri("http://localhost:8088/api/survey")
+                .setEntity(stringE)
+                .build();
 
-            StringEntity stringE = new StringEntity("{\"creation_date\": \"creation_date\", " +
-                    "\"title\": \"title\", " +
-                    "\"user_id\": 1, " +
-                    "\"elements\": null}");
-            stringE.setContentType("application/json;charset=utf-8");
-            request = RequestBuilder.create("POST")
-                    .setUri("http://localhost:8088/api/survey")
-                    .setEntity(stringE)
-                    .build();
-
-//        When
+    //When
         HttpResponse response = null;
         try {
             response = HttpClientBuilder.create().build().execute(request);
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        Then
-        assertEquals( HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
 
+    //Then
+        String responseString = EntityUtils.toString(response.getEntity());
+        //Testing the Status Code
+        assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode(), "Testing the Status Code failed");
+        //Testing the Media Type
+        assertEquals("application/json", ContentType.getOrDefault(response.getEntity()).getMimeType(), "Testing the Media Type failed");
+        //Testing the JSON Payload
+        assertTrue(responseString.matches("[0-9]+"));
         //ToDo check for correct and incorrect auth (not possible yet)
 
+        //delete
+        if(responseString.matches("[0-9]+"))
+            surveyService.deleteSurveybyId(Integer.parseInt(responseString));
     }
-    @After
-    //ToDo
-
 
 
     @Test
@@ -119,6 +142,14 @@ class SurveyControllerTest {
 //       Then
         assertEquals( HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
     }
-    @After
-    //ToDo
+
+    public static <T> T retrieveResourceFromResponse(HttpResponse response, Class<T> tClass)
+            throws IOException {
+
+        String jsonFromResponse = EntityUtils.toString(response.getEntity());
+        ObjectMapper mapper = new ObjectMapper();
+                //.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper.readValue(jsonFromResponse, tClass);
+    }
+
 }
