3314e2af927a1428abfbc4b0a296a45d8d8a9870
janniksinz <j.sinz@gmx.de>
2021-04-26 22:10:45 +0200

    init

---
+++ src/main/java/com/partycipate/Partycipate/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.partycipate.Partycipate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/","/other-resources/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+}
