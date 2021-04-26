d8a3959f4783a908f7e13935c2272bafdddb1089
janniksinz <j.sinz@gmx.de>
2021-04-26 22:21:28 +0200

    fix

--- src/main/java/com/partycipate/Partycipate/config/MvcConfig.java
+++ src/main/java/com/partycipate/Partycipate/config/MvcConfig.java
@@ -1,12 +1,6 @@
 package com.partycipate.Partycipate.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-
-@Configuration
+/*@Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
     @Override
@@ -18,4 +12,4 @@ public class MvcConfig implements WebMvcConfigurer {
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
     }
-}
+}*/
