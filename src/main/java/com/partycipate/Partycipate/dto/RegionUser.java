b59dfc50d528680f4b51c24973bcd036d72e9d5f
wi19225 <wi19225@lehre.dhbw-stuttgart.de>
2021-04-29 21:48:54 +0200

    comments got cleaned up

--- src/main/java/com/partycipate/Partycipate/dto/RegionUser.java
+++ src/main/java/com/partycipate/Partycipate/dto/RegionUser.java
@@ -4,26 +4,4 @@ public interface RegionUser {
 
     String getId();
     Long getValue();
-
-
-    /*public RegionUser(String id, Long v) {
-        this.id = id;
-        this.v = v;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Long getV() {
-        return v;
-    }
-
-    public void setV(Long v) {
-        this.v = v;
-    }*/
 }
