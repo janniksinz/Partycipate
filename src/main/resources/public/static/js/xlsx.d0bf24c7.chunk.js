dc64aa7801238e7669af32399e7649111e197e0c
Marlo Kessler <marlo.kessler@connapptivity.de>
2021-05-05 11:59:40 +0200

    feature: redirect to frontend error page on error; add latest version of frontend

--- src/main/resources/public/static/js/xlsx.d0bf24c7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpadmin_app=this.webpackJsonpadmin_app||[]).push([[153],{381:function(n,p){},493:function(n,p){},494:function(n,p){}}]);
-//# sourceMappingURL=xlsx.d0bf24c7.chunk.js.map
