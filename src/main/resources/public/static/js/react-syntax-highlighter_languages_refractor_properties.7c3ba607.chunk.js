64454d6a76d42e6ec0eb710bbd04908301c67483
Marlo Kessler <marlo.kessler@connapptivity.de>
2021-05-06 11:00:11 +0200

    Add latest frontend version

---
+++ src/main/resources/public/static/js/react-syntax-highlighter_languages_refractor_properties.7c3ba607.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpadmin_app=this.webpackJsonpadmin_app||[]).push([[106],{590:function(t,n,s){"use strict";function a(t){t.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}t.exports=a,a.displayName="properties",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.7c3ba607.chunk.js.map
