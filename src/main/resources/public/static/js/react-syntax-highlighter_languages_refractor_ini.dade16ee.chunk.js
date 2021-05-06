9a38738aab266f43af3adbcbeabca0f5f86e0fea
Marlo Kessler <marlo.kessler@connapptivity.de>
2021-05-06 10:26:44 +0200

    feature: insert latest frontend files

---
+++ src/main/resources/public/static/js/react-syntax-highlighter_languages_refractor_ini.dade16ee.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpadmin_app=this.webpackJsonpadmin_app||[]).push([[60],{550:function(t,n,a){"use strict";function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.dade16ee.chunk.js.map
